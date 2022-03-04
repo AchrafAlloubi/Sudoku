0cad67272c3700901ae608eaf01ea612026d75f4
Achraf ALLOUBI <achrafalloubi@gmail.com>
2022-03-04 05:43:09 -0500

    MRV done

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class main:
         self.contraintes = {}
         # liste de toutes les cases, chaque case est definie de :
         # valeur de la case, nbre de valeur dispo pour la case, la liste des valeurs dispo pour la case
-        self.assignment = [[[0, 0, []] for i in range(taille * taille)] for j in range(taille * taille)]
+        self.gride = [[[0, 0, []] for i in range(taille * taille)] for j in range(taille * taille)]
 
     # todo contraintes binaires
     def creer_contrainte(self):
@@ -51,8 +51,8 @@ class main:
     def afficher_grille(self):
         pass
 
-    # todo algo restants : MRV, degree heuristic, AC3
-    # MRV => on va parcourir l'ensemble des cases declarées dans la var self.assignment et on recupere la case
+    # todo algo restant : AC3
+    # MRV => on va parcourir l'ensemble des cases declarées dans la var self.gride et on recupere la case
     #dont le domaine est le plus petit
     #degree heuristic => doit retourner la case possedant le plus grand nombre de contraintes binaires
     #Pseudo code dans le cours
@@ -62,7 +62,7 @@ class main:
     def recursive_backtracking(self):
 
         if self.verifier_completude():
-            return self.assignment
+            return self.gride
         x, y = self.Select_Unasigned_Variable()
 
         # Boucle de la technique least constraining value
@@ -76,70 +76,23 @@ class main:
                         return result
                     self.enlever_valeur(x,y,valeur)
 
-    # add value dans assignment + modification du domaine des valeurs possibles possedant contraintes binaires avec la case en question
-    def ajouter_valeur(self, x, y, valeur):
+        #Boucle du MRV + degree-heuristic
+        else:
+            for valeur in self.gride[x][y][2]:
+                if self.consistant(x, y, valeur):
+                    self.ajouter_valeur(x,y,valeur)
+                    resultat = self.recursive_backtracking()
 
-        self.assignment[x][y][0] = valeur
-        self.assignment[x][y][1] = 1
-        self.assignment[x][y][2] = [valeur]
-        key = "[" + str(x) + "," + str(y) + "]"
-        liste_contraintes = self.contraintes[key]
-
-        for element in liste_contraintes:
-            if valeur in self.assignment[element[0]][element[1]][2]:
-                self.assignment[element[0]][element[1]][2].enlever_valeur(valeur)
-                self.assignment[element[0]][element[1]][1] -= 1
-
-
-    def enlever_valeur(self, x, y, valeur):
-
-        self.assignment[x][y][0] = 0
-        key = "[" + str(x) + "," + str(y) + "]"
-        liste_contraintes = self.contraintes[key]
-        domainesLocaux = copy.copy(self.domaines)
-
-        for element in liste_contraintes:
-            if self.assignment[element[0]][element[1]][0] != 0 and self.assignment[element[0]][element[1]][0] in domainesLocaux:
-                domainesLocaux.enlever_valeur(self.assignment[element[0]][element[1]][0])
-
-        self.assignment[x][y][2] = domainesLocaux
-        self.assignment[x][y][1] = len(domainesLocaux)
-
-        for element in liste_contraintes:
-            if self.assignment[element[0]][element[1]][0] == 0:
-                if valeur not in self.assignment[element[0]][element[1]][2]:
-                    second_key = "[" + str(element[0]) + "," + str(element[1]) + "]"
-                    second_liste_contraintes = self.contraintes[second_key]
-                    add_valeur = True
-                    for second_element in second_liste_contraintes:
-                        if valeur == self.assignment[second_element[0]][second_element[1]][0]:
-                            add_valeur = False
-                    if add_valeur:
-                        self.assignment[element[0]][element[1]][2].append(valeur)
-                        self.assignment[element[0]][element[1]][1] += 1
-
-
-    def leastConstrainingValue(self, x, y):
-        valeurs = []
-        for v in self.domaines:
-            valeurs.append([v, 0])
-        contraintes = self.contraintes["[" + str(x) + "," + str(y) + "]"]
-
-        for v in valeurs:
-            i = 0
-            for c in contraintes:
-                if self.assignment[c[0]][c[1]][0] == 0 and v[0] in self.assignment[c[0]][c[1]][2]:
-                    i = i + 1
-            v[1] = i
-
-        valeurs.sort(key=lambda x: x[1])
-        return valeurs
+                    if resultat != False:
+                        return resultat
+                    self.enlever_valeur(x,y,valeur)
+        return False
 
 
     # Check si la grille est full, le 0 signifie que le case est vide
     def verifier_completude(self):
         full = True
-        for ligne in self.assignment:
+        for ligne in self.gride:
             for case in ligne:
                 if case[0] == 0:
                     full = False
@@ -154,12 +107,27 @@ class main:
             caseSelectionnee = [[0, 0], self.taille * self.taille + 1]
             for x in range(0, self.taille * self.taille):
                 for y in range(0, self.taille * self.taille):
-                    if self.assignment[x][y][0] == 0:
-                        caseSelectionnee = [[x, y], self.assignment[x][y][1]]
+                    if self.gride[x][y][0] == 0:
+                        caseSelectionnee = [[x, y], self.gride[x][y][1]]
                         return caseSelectionnee[0][0], caseSelectionnee[0][1]
+
+        elif self.technique == "mrv":
+            caseSelectionnee = [[0,0], self.taille*self.taille + 1]
+            for x in range(0,self.taille*self.taille):
+                for y in range(0,self.taille*self.taille):
+
+                    if self.gride[x][y][0] == 0:
+                        if self.gride[x][y][1] < caseSelectionnee[1]:
+                            caseSelectionnee = [[x,y], self.gride[x][y][1]]
+
+        #todo boucle degree heuristic
+        elif self.technique == "degree heuristic":
+            pass
 
 
         return caseSelectionnee[0][0], caseSelectionnee[0][1]
+
+
 
     #verifier si la valeur donnée en param existe dans le domaine de valeurs possibles pour la case
     def consistant(self, x, y, valeur):
@@ -170,7 +138,66 @@ class main:
         key = "[" + str(x) + "," + str(y) + "]"
         liste_contrainte = self.contrainte[key]
 
-        if valeur in self.assignment[x][y][2]:
+        if valeur in self.gride[x][y][2]:
             return True
 
         return False
+
+
+    # add value dans gride + modification du domaine des valeurs possibles possedant contraintes binaires avec la case en question
+    def ajouter_valeur(self, x, y, valeur):
+        self.gride[x][y][0] = valeur
+        self.gride[x][y][1] = 1
+        self.gride[x][y][2] = [valeur]
+        key = "[" + str(x) + "," + str(y) + "]"
+        liste_contraintes = self.contraintes[key]
+
+        for element in liste_contraintes:
+            if valeur in self.gride[element[0]][element[1]][2]:
+                self.gride[element[0]][element[1]][2].enlever_valeur(valeur)
+                self.gride[element[0]][element[1]][1] -= 1
+
+
+    def enlever_valeur(self, x, y, valeur):
+        self.gride[x][y][0] = 0
+        key = "[" + str(x) + "," + str(y) + "]"
+        liste_contraintes = self.contraintes[key]
+        domainesLocaux = copy.copy(self.domaines)
+
+        for element in liste_contraintes:
+            if self.gride[element[0]][element[1]][0] != 0 and self.gride[element[0]][element[1]][
+                0] in domainesLocaux:
+                domainesLocaux.enlever_valeur(self.gride[element[0]][element[1]][0])
+
+        self.gride[x][y][2] = domainesLocaux
+        self.gride[x][y][1] = len(domainesLocaux)
+
+        for element in liste_contraintes:
+            if self.gride[element[0]][element[1]][0] == 0:
+                if valeur not in self.gride[element[0]][element[1]][2]:
+                    second_key = "[" + str(element[0]) + "," + str(element[1]) + "]"
+                    second_liste_contraintes = self.contraintes[second_key]
+                    add_valeur = True
+                    for second_element in second_liste_contraintes:
+                        if valeur == self.gride[second_element[0]][second_element[1]][0]:
+                            add_valeur = False
+                    if add_valeur:
+                        self.gride[element[0]][element[1]][2].append(valeur)
+                        self.gride[element[0]][element[1]][1] += 1
+
+
+    def leastConstrainingValue(self, x, y):
+        valeurs = []
+        for v in self.domaines:
+            valeurs.append([v, 0])
+        contraintes = self.contraintes["[" + str(x) + "," + str(y) + "]"]
+
+        for v in valeurs:
+            i = 0
+            for c in contraintes:
+                if self.gride[c[0]][c[1]][0] == 0 and v[0] in self.gride[c[0]][c[1]][2]:
+                    i = i + 1
+            v[1] = i
+
+        valeurs.sort(key=lambda x: x[1])
+        return valeurs
